a324e53a1d7932338f28f4c5852607376279f08f
Skyler <skyler.badge@gmail.com>
2018-08-07 18:18:20 +0530

    Added Do not serve and Cart add layouts

---
+++ app/src/main/java/com/iitd/qriee/CartAdd.java
@@ -0,0 +1,13 @@
+package com.iitd.qriee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_add);
+    }
+}
